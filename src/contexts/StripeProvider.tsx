1122fe0a839529725133f5a632d7cfea71ccebcb
maitrucquynh <97218479+quynhmido@users.noreply.github.com>
2025-01-08 16:22:23 +0700

    Fix Stripe Provider

--- src/contexts/StripeProvider.tsx
+++ src/contexts/StripeProvider.tsx
@@ -1,11 +1,11 @@
 // StripeProvider.js
+"use client"
 import { ReactNode } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
-  process.env.REACT_APP_STRIPE_PUBLIC_KEY ||
-    "pk_test_51QdA3qD5GjALdetb59zRfoomZ25cbJQ7GvspuHKi9EKsAq3ySAIdhNIuqItzdhm0ON2n0lAfabvCYbiDpduGHEwZ00eShWCDGX"
+  "pk_test_51QdA3qD5GjALdetb59zRfoomZ25cbJQ7GvspuHKi9EKsAq3ySAIdhNIuqItzdhm0ON2n0lAfabvCYbiDpduGHEwZ00eShWCDGX"
 );
 
 const StripeProvider = ({ children }: { children: ReactNode }) => {
