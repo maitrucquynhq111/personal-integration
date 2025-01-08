6e3dac044bfa40f587d8dc406b3247e42ef40977
maitrucquynh <97218479+quynhmido@users.noreply.github.com>
2025-01-08 16:15:26 +0700

    Add Stripe Provider

---
+++ src/contexts/StripeProvider.tsx
@@ -0,0 +1,15 @@
+// StripeProvider.js
+import { ReactNode } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.REACT_APP_STRIPE_PUBLIC_KEY ||
+    "pk_test_51QdA3qD5GjALdetb59zRfoomZ25cbJQ7GvspuHKi9EKsAq3ySAIdhNIuqItzdhm0ON2n0lAfabvCYbiDpduGHEwZ00eShWCDGX"
+);
+
+const StripeProvider = ({ children }: { children: ReactNode }) => {
+  return <Elements stripe={stripePromise}>{children}</Elements>;
+};
+
+export default StripeProvider;
